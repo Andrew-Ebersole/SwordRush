fe608d0595a68c5e137db7a1bbe72486daf9d6fc
Joshua Leong <jkl7075@rit.edu>
2023-03-10 15:41:27 -0500

    added position x y properties to game object, added white rectangle, and added walls and collision logic (does not work needs fixing)

--- SwordRush/GameManager.cs
+++ SwordRush/GameManager.cs
@@ -18,19 +18,27 @@ namespace SwordRush
         public event ToggleGameState gameOver;
         private Texture2D dungeontilesTexture2D;
         private Rectangle window;
+        private List<SceneObject> walls;
 
 
 
         // --- Constructor --- //
 
-        public GameManager(ContentManager content, Point windowSize)
+        public GameManager(ContentManager content, Point windowSize, Texture2D whiteRectangle)
         {
             this.spriteSheet = spriteSheet;
             gameActive = false;
             enemies = new List<Enemy>();
-            player = new Player(null, new Rectangle(10, 10, 16, 32));
+            player = new Player(null, new Rectangle(500, 500, 16, 32));
             enemies.Add(new ShortRangeEnemy(null, new Rectangle(10, 10, 16, 16), player));
             dungeontilesTexture2D = content.Load<Texture2D>("DungeonTiles");
+            walls = new List<SceneObject>();
+
+            //temporary test walls
+            
+            walls.Add(new SceneObject(true, whiteRectangle, new Rectangle(1000, 500, 20, 20)));
+
+
             this.window = new Rectangle(0, 0,
                 windowSize.X, windowSize.Y);
         }
@@ -47,12 +55,19 @@ namespace SwordRush
                 foreach (Enemy enemy in enemies)
                 {
                     enemy.Update(gt);
+
+                    //update enemy collision
+                    Collision(enemy, walls);
                 }
 
                 if (player.Health <= 0)
                 {
                     gameOver();
                 }
+
+                //update player collision
+                Collision(player, walls);
+                
             }
         }
 
@@ -64,11 +79,80 @@ namespace SwordRush
 
                 sb.Draw(dungeontilesTexture2D, player.Rectangle, new Rectangle(128, 64, 16, 32), Color.White);
                 sb.Draw(dungeontilesTexture2D, enemies[0].Rectangle, new Rectangle(368, 80, 16, 16), Color.White);
+
+                //draw walls
+                foreach (SceneObject obj in walls)
+                {
+                    obj.Draw(sb);
+                }
             }
         }
         public void GenerateRoom()
         {
+
         }
+
+        public void Collision(GameObject entity, List<SceneObject> walls)
+        {
+            //temporary variables for calculations
+            Rectangle entityRect = entity.Rectangle;
+            List<Rectangle> intersectionsList = new List<Rectangle>();
+            List<Rectangle> wallRects = new List<Rectangle>();
+
+            foreach (SceneObject obj in walls)
+            {
+                wallRects.Add(obj.Rectangle);
+            }
+
+            //get list of intersections
+            for (int i = 0; i < walls.Count; i++)
+            {
+                if (entityRect.Intersects(wallRects[i]))
+                {
+                    intersectionsList.Add(wallRects[i]);
+                }
+            }
+
+            //collision
+
+            //loop through intersects
+            for (int i = 0; i < intersectionsList.Count; i++)
+            {
+                Rectangle intersect = Rectangle.Intersect(entityRect, intersectionsList[i]);
+
+                //horizontal collision
+                if (intersect.Width < intersect.Height)
+                {
+                    if (entityRect.X < intersect.X)
+                    {
+                        entityRect.X -= intersect.Width;
+                    }
+                    else
+                    {
+                        entityRect.X += intersect.Width;
+                    }
+                }
+                //vertical collision
+                else
+                {
+                    if (entityRect.Y < intersect.Y)
+                    {
+                        entityRect.Y -= intersect.Height;
+                    }
+                    else
+                    {
+                        entityRect.Y += intersect.Height;
+                    }
+                    
+                }
+            }
+
+            //update player position
+            entity.X = entityRect.X;
+            entity.Y = entityRect.Y;
+
+        }
+
 
         public void StartGame()
         {
