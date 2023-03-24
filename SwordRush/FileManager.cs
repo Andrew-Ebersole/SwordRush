1f8ccce86e1d31344dd3727b2f1b2ac8f3fec8d0
Joshua Leong <jkl7075@rit.edu>
2023-03-24 15:53:07 -0400

    started load grid method in file manager

--- SwordRush/FileManager.cs
+++ SwordRush/FileManager.cs
@@ -15,5 +15,20 @@ namespace SwordRush
             writer.WriteLine(path);
             writer.Close();
         }
+
+        /// <summary>
+        /// creates a new grid based on the data read from a file
+        /// </summary>
+        /// <param name="path">the file path of the file being used to load the data</param>
+        /// <returns>a 2D int array that contains the grid data</returns>
+        public int[,] LoadGrid(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+            int[,] grid = null;
+
+
+
+
+        }
     }
 }
