a7d01b4f39efb6544345c22a74bde014c9b5a1c5
Weijie YE <wy8933@rit.edu>
2023-04-07 13:25:01 -0400

    deleted AStar

--- SwordRush/AStarNode.cs
+++ SwordRush/AStarNode.cs
@@ -5,26 +5,6 @@ namespace SwordRush
 {
     internal class AStarNode
     {
-        public Point Position { get; set; }
-        public AStarNode Parent { get; set; }
-        public int G { get; set; }
-        public int H { get; set; }
-        public int F { get; set; }
-        public List<AStarNode> Neighbors { get; set; }
-
-        public AStarNode(Point position)
-        {
-            Position = position;
-            G = 0;
-            H = 0;
-            Parent = null;
-        }
-
-        public float CalculateH(AStarNode goal)
-        {
-            return Vector2.Distance(Position.ToVector2(), goal.Position.ToVector2());
-        }
-
 
     }
 }
