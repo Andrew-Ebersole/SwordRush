7a375bfcb8f7bfde1bda7af7adb5fd03858e5148
Weijie YE <wy8933@rit.edu>
2023-04-17 12:31:04 -0400

    added click sound effect to menu

--- SwordRush/TextButton.cs
+++ SwordRush/TextButton.cs
@@ -69,6 +69,7 @@ namespace SwordRush
                 && previousMState.LeftButton == ButtonState.Released)
             {
                 leftClicked = true;
+                SoundManager.Get.ClickEffect.Play();
             }
             else
             {
