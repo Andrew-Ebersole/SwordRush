498f46a73c063fb2d86b43d2f98833c5feb2d0f5
unknown <laoxu@vrymux.com>
2023-04-22 19:46:54 -0400

    updated effect

--- SwordRush/PlayerEffectScreen.cs
+++ SwordRush/PlayerEffectScreen.cs
@@ -11,10 +11,10 @@ public class PlayerEffectScreen
 
     public PlayerEffectScreen(Point windowSize)
     {
-        _animation.AddFrame(new Color(40, 0, 0, 20));
-        _animation.AddFrame(new Color(40, 0, 0, 20));
-        _animation.AddFrame(new Color(20, 0, 0, 10));
-        _animation.AddFrame(new Color(15, 0, 0, 5));
+        _animation.AddFrame(new Color(40, 0, 0, 100));
+        _animation.AddFrame(new Color(40, 0, 0, 100));
+        _animation.AddFrame(new Color(20, 0, 0, 100));
+        _animation.AddFrame(new Color(15, 0, 0, 60));
         _animation.AddFrame(new Color(0, 0, 0, 0));
 
         _windowSize = windowSize;
